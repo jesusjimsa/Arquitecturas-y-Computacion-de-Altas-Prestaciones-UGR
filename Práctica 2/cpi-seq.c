070985efd15f3d636ad4820d03828df20301c238
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-22 12:19:00 +0100

    cpi secuencial

--- Práctica 2/cpi-seq.c
+++ Práctica 2/cpi-seq.c
@@ -3,37 +3,39 @@
 #include <math.h>
 
 int main(int argc, char **argv){
-  register double width, sum;
-  register int intervals, i;
-  register double diferencia;
-  register double error;
-  const double PI = 3.14159265358979323846264338327950288419716939937510;
-  
-  FILE *out_file = fopen("tiempos.dat", "w");
+	if(argc != 3){
+		printf("Tienes que escribir dos argumentos: número de intervalos y error\n");
+		printf("Ejemplo: ./cpi-seq 1000 0.5\n");
 
-  /* get the number of intervals */
-  intervals = atoi(argv[1]);
-  error = atof(argv[2]);
-  width = 1.0 / intervals;
+		exit(-1);
+	}
 
-  /* do the computation */
-  sum = 0;
-  
-  for (i = 0; i < intervals; ++i) {
-    register double x = (i + error) * width;
-    
-    sum += 4.0 / (1.0 + x * x);
-  }
-  
-  sum *= width;
-  
-  diferencia = sum - PI;
+	register double width, sum;
+	register int intervals, i;
+	register double diferencia;
+	register double error;
+	const double PI = 3.14159265358979323846264338327950288419716939937510;
+	
+	/* get the number of intervals */
+	intervals = atoi(argv[1]);
+	error = atof(argv[2]);
+	width = 1.0 / intervals;
 
-  printf("Estimation of pi is %0.50f\n", sum);
-  printf("Diferencia: %0.50f\n", fabs(diferencia));
-  
-  fprintf(out_file, "%0.50f\n", fabs(diferencia));
+	/* do the computation */
+	sum = 0;
+	
+	for (i = 0; i < intervals; ++i) {
+		register double x = (i + error) * width;
+		
+		sum += 4.0 / (1.0 + x * x);
+	}
+	
+	sum *= width;
+	diferencia = sum - PI;
 
-  return(0);
+	printf("Estimation of pi is\t%0.50f\n", sum);
+	printf("Diferencia:\t\t%0.50f\n", fabs(diferencia));
+
+	return(0);
 }
 
