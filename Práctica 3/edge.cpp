9b7497de1e51a7a4ce6c183c9817fcf5d16f7a3f
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-04-11 01:06:08 +0200

    Se puede modificar una imagen

--- Práctica 3/edge.cpp
+++ Práctica 3/edge.cpp
@@ -4,23 +4,29 @@
 using namespace std;
 using namespace cimg_library;
 
-int main(){
-	CImg<int> img("paul.jpg"); 
-	//CImgDisplay main_disp(img,"Click a point");
-	// int r = img(10, 10, 0, 0);
-	// cout << r << endl;
-	// cout << img.height() << ", " << img.width() << endl;
-	//while (!main_disp.is_closed());
-	// Get width, height, number of channels
-	int w=img.width();
-	int h=img.height();
-	int c=img.spectrum();
-	cout << "Dimensions: " << w << "x" << h << " " << c << " channels" <<endl;
+CImg<int> imgToGray(CImg<int> img){
+	// TODO
+}
 
-	// Dump all pixels
-	// for(int y=0;y<h;y++){
-	//    for(int x=0;x<w;x++){
-	// 	  cout << y << "," << x << " " << (int)img(x,y) << endl;
-	//    }
-	// }
+int gaussianKernel(int size){
+	// TODO
+}
+
+CImg<int> sobelFilter(CImg<int> img){
+	// TODO
+}
+
+int main(){
+	CImg<int> img("paul.jpg");
+
+	for(int i = 0; i < 600; i++){
+		for(int j = 0; j < 300; j++){
+			img(i, j, 0) = 0;
+			//img(i, j, 1) = 0;
+			//img(i, j, 2) = 0;
+		}
+	}
+
+	CImg<int> jeje(img);
+	jeje.save("jeje.jpg");
 }
