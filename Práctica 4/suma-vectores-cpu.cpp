d36a29d42089d7feba3bac3024a536b653629af4
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-05-05 21:20:27 +0200

    Suma de vectores en la CPU

--- Práctica 4/suma-vectores-cpu.cpp
+++ Práctica 4/suma-vectores-cpu.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
 
 using namespace std;
 
+void readfile(vector<float> &vec, string filename){
+	ifstream file;
+	
+	file.open(filename);
+
+	for(string line; getline(file, line);){
+		vec.push_back(stof(line));
+	}
+
+	vec.erase(vec.begin());
+
+	file.close();
+}
+
+vector<float> suma_vectores(vector<float> first, vector<float> second){
+	vector<float> result;
+
+	if(first.size() == second.size()){
+		for(int i = 0; i < first.size(); i++){
+			result.push_back(first[i] + second[i]);
+		}
+	}
+
+	return result;
+}
+
 int main(){
-	cout << "Hola mundo" << endl;
+	vector<float> first, second, result;
+	clock_t begin, end;
+	
+	readfile(first, "data/9/input0.raw");
+	readfile(second, "data/9/input1.raw");
+
+	begin = clock();
+
+	result = suma_vectores(first, second);
+
+	end = clock();
+
+	for(int i = 0; i < result.size(); i++){
+		cout << result[i] << endl;
+	}
+
+	cout << "Tiempo: " << double(end - begin) / CLOCKS_PER_SEC << " segundos" << endl;
 }
