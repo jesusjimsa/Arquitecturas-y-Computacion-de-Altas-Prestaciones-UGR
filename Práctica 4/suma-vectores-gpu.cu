9853d3bbc3f434a127ac088df21c646d4b8b63a5
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-05-17 13:46:01 +0200

    Copia correcta de datos en los array
    #11

--- Práctica 4/suma-vectores-gpu.cu
+++ Práctica 4/suma-vectores-gpu.cu
@@ -36,6 +36,7 @@ void properties(){
 		cout << "memory clock rate (KHz): " << dev.memoryClockRate << endl;
 		cout << "L2 cache size (bytes): " << dev.l2CacheSize << endl;
 		cout << "max threads per SM: " << dev.maxThreadsPerMultiProcessor << endl;
+		cout << endl;
 	}
 }
 
@@ -61,10 +62,10 @@ void readfile(vector<float> &vec, string filename){
 
 // Kernel function to add the elements of two arrays
 __global__
-void add(float *x, float *y, float *result, int size){
+void add(float *x, float *y, float *result, int *size){
 	int i = threadIdx.x + blockDim.x * blockIdx.x;
 
-	if(i < size){
+	if(i < (*size)){
 		result[i] = x[i] + y[i];
 	}
 }
@@ -72,11 +73,11 @@ void add(float *x, float *y, float *result, int size){
 int main(void){
 	clock_t begin, end;
 	vector<float> vec;
+	int mem_vec_size, *gpu_vec_size, *mem_pointer_size;
 	float *memoria_x, *memoria_y;
 	float *gpu_x, *gpu_y;
 	float *memoria_result, *gpu_result;
 	ofstream file;
-
 
 	memoria_x = NULL;
 	memoria_y = NULL;
@@ -85,55 +86,71 @@ int main(void){
 	memoria_x = NULL;
 	gpu_y = NULL;
 
+	mem_pointer_size = (int*)malloc(sizeof(int));
+
 	// Imprimir características
 	properties();
 
 	readfile(vec, "data/9/input0.raw");
 
+	mem_vec_size = vec.size();
+
 	// Reservar memoria para el primer array
-	cudaMallocManaged(&memoria_x, vec.size()*sizeof(float));
-	cudaMallocManaged(&gpu_x, vec.size()*sizeof(float));
+	cudaMallocManaged(&memoria_x, mem_vec_size*sizeof(float));
+	cudaMallocManaged(&gpu_x, mem_vec_size*sizeof(float));
 
 	for(int i = 0; i < vec.size(); i++){
 		memoria_x[i] = vec[i];
 	}
 
+	vec.clear();
+
 	readfile(vec, "data/9/input1.raw");
 
+	mem_vec_size = vec.size();
+
 	// Reservar memoria para el segundo array
-	cudaMallocManaged(&memoria_y, vec.size()*sizeof(float));
-	cudaMallocManaged(&gpu_y, vec.size()*sizeof(float));
+	cudaMallocManaged(&memoria_y, mem_vec_size*sizeof(float));
+	cudaMallocManaged(&gpu_y, mem_vec_size*sizeof(float));
 
 	for(int i = 0; i < vec.size(); i++){
 		memoria_y[i] = vec[i];
 	}
 
 	// Reservar memoria para el array resultante
-	cudaMallocManaged(&memoria_result, vec.size()*sizeof(float));
-	cudaMallocManaged(&gpu_result, vec.size()*sizeof(float));
+	cudaMallocManaged(&memoria_result, mem_vec_size*sizeof(float));
+	cudaMallocManaged(&gpu_result, mem_vec_size*sizeof(float));
+
+	// Reservar memoria para el dato del tamaño del vector
+	cudaMallocManaged(&gpu_vec_size, sizeof(int));
+
+	*mem_pointer_size = mem_vec_size;
 
 	// Copiar los datos en la GPU
-	cudaMemcpy(gpu_x, memoria_x, sizeof(float)*vec.size(), cudaMemcpyHostToDevice);
-	cudaMemcpy(gpu_y, memoria_y, sizeof(float)*vec.size(), cudaMemcpyHostToDevice);
-	cudaMemcpy(gpu_result, memoria_result, sizeof(float)*vec.size(), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_x, memoria_x, sizeof(float)*mem_vec_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_y, memoria_y, sizeof(float)*mem_vec_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_result, memoria_result, sizeof(float)*mem_vec_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_vec_size, mem_pointer_size, sizeof(int), cudaMemcpyHostToDevice);
 
 	begin = clock();
 
 	// Llamar al kernel
 	// <<< Número de bloques, número de hebras >>>
-	add<<< (int)(vec.size()/256)+1, (int)vec.size() >>>(gpu_x, gpu_y, gpu_result, vec.size());
+	add<<< (int)(mem_vec_size/256)+1, (int)mem_vec_size >>>(gpu_x, gpu_y, gpu_result, gpu_vec_size);
 
 	// Esperar a que la GPU termine
 	cudaDeviceSynchronize();
 
 	// Copiar los resultados en memoria
-	cudaMemcpy(memoria_result, gpu_result, sizeof(float)*vec.size(), cudaMemcpyDeviceToHost);
+	cudaMemcpy(memoria_result, gpu_result, sizeof(float)*mem_vec_size, cudaMemcpyDeviceToHost);
 
 	end = clock();
 
 	file.open("result.raw");
 
-	for(int i = 0; i < vec.size(); i++){
+	file << mem_vec_size << endl;
+
+	for(int i = 0; i < mem_vec_size; i++){
 		file << memoria_result[i] << endl;
 	}
 
@@ -148,6 +165,9 @@ int main(void){
 	cudaFree(gpu_x);
 	cudaFree(gpu_y);
 	cudaFree(gpu_result);
+	cudaFree(gpu_vec_size);
+	
+	free(mem_pointer_size);
 
 	return 0;
 }
